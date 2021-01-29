ba97992a5135e53346d95b53b5e69f1c40ee2873
Angelo Shema <geoangercola@gmail.com>
2021-01-29 14:00:52 +0200

    Add welcome message in console.log

---
+++ js/welcome.js
@@ -0,0 +1,9 @@
+console.log(`
+┌──────────────────────────┐
+│ Hello there!             │
+│ I can see you hacking    │
+├──────────────────────────┤
+│ geoangercola@gmail.com   │
+│ vixalien.io              │
+└──────────────────────────┘
+`)
