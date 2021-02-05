075d8abbf0ca002d4635c8b757ae6b4fb9e66aa3
vixalien <37999241+vixalien@users.noreply.github.com>
2021-02-05 02:20:04 +0200

    Create generate-sitemap.js

---
+++ src/generate-sitemap.js
@@ -0,0 +1,20 @@
+let fs = require('fs');
+let { resolve } = require('path');
+let glob = require('glob');
+
+let allPages = glob(resolve('out/**/*'));
+
+let siteMap = `<?xml version="1.0" encoding="UTF-8"?>
+
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+   ${allPages.map(page => `
+   <sitemap>
+
+      <loc>https://www.vixalien.ga/${page.replace(/^out\//, '')}</loc>
+
+   </sitemap>
+`)}
+</sitemapindex>`
+
+fs.writeFileSync(resolve('out', 'sitemap.xml'));
