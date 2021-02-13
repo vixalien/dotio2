f96f08a0afcce3442c83dbe4ae70203c72152cb1
Angelo Shema <geoangercola@gmail.com>
2021-02-14 01:50:39 +0200

    Simplify canonical to use process.env.URL only

--- components/canonical.js
+++ components/canonical.js
@@ -1,13 +1,18 @@
 import Explosiv from 'explosiv'
 
+let origin = () => {
+	// let origin;
+	// if (process.env.VERCEL) {
+	// 	origin = "https://" + process.env.VERCEL_URL
+	// } else {
+	// 	origin = process.env.URL
+	// }
+	return process.env.URL
+}
+
 let Canonical = ({ path }) => {
-	let origin;
-	if (process.env.VERCEL) {
-		origin = "https://" + process.env.VERCEL_URL
-	} else {
-		origin = process.env.URL
-	}
-	return <link rel="canonical" href={origin+path}/>
+	return <link rel="canonical" href={origin()+path}/>
 }
 
 export default Canonical;
+export { origin };
