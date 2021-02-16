972b90774f0fff04c61d2378b8be073d2ee387ab
Angelo Shema <geoangercola@gmail.com>
2021-02-16 04:57:46 +0200

    Fix mixed content analytics

--- pages/_document.js
+++ pages/_document.js
@@ -4,17 +4,14 @@ let Document = () => {
 			<meta charset="utf-8"/>
 			<meta name="viewport" content="width=device-width,initial-scale=1"/>
 			<meta name="theme-color" content="#0a1826" />
+			<link rel="stylesheet" href="/css/app.css"/>
 			<link rel="shortcut icon" href="/favicon.ico"/>
 			<link rel="manifest" href="/manifest.min.json"/>
-			<link
-				rel="apple-touch-icon"
-				sizes="180x180"
-				href="/favicon/android-chrome-192x192.png"
-			/>
+			<link rel="apple-touch-icon"sizes="180x180"href="/favicon/android-chrome-192x192.png"/>
 			<link rel="alternate" type="application/rss+xml" title="RSS Feed for vixalien.ga" href="https://vixalien.ga/feed/feed.rss" />
 			<link rel="alternate" type="application/atom+xml" title="Atom Feed for vixalien.ga" href="https://vixalien.ga/feed/feed.atom" />
 			<link rel="alternate" type="application/json" title="JSON Feed for vixalien.ga" href="https://vixalien.ga/feed/feed.json" />
-			<script async src="://www.googletagmanager.com/gtag/js?id=G-YTE49ZSFG1"></script>
+			<script async src="https://www.googletagmanager.com/gtag/js?id=G-YTE49ZSFG1"></script>
 			<script html={`window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-YTE49ZSFG1');`}/>
 		</head>
 		<body>
