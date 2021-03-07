4864066246e36de4fef96a91158fd5b6c009a131
Angelo Shema <geoangercola@gmail.com>
2021-03-07 17:52:59 +0200

    Fix offline indicator not aligning with content

--- js/show_offline_link.js
+++ js/show_offline_link.js
@@ -1,11 +1,10 @@
 if (!navigator.onLine && !location.href.match('offline')) {
 	const offline = document.createElement('aside')
 	offline.style.padding = 'var(--padding-top) var(--padding-right) var(--padding-bottom) var(--padding-left)'
+	offline.className = "container";
 	const text = document.createElement('p');
 	
 	text.appendChild(document.createTextNode('Seems like you are offline. Visit '));
-	text.style.padding = "padding: var(--padding-top) var(--padding-right) var(--padding-bottom) var(--padding-left)";
-	text.className = "container"
 	
 	const link = document.createElement('a');
 	link.innerHTML = link.href = '/offline';
