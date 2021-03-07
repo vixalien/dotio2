bd106907fdb23f08d8bd911309247ac0e33aa77c
Angelo Shema <geoangercola@gmail.com>
2021-03-07 17:49:40 +0200

    Create a container paragraph to show that you are offline

--- js/show_offline_link.js
+++ js/show_offline_link.js
@@ -4,11 +4,13 @@ if (!navigator.onLine && !location.href.match('offline')) {
 	const text = document.createElement('p');
 	
 	text.appendChild(document.createTextNode('Seems like you are offline. Visit '));
+	text.style.padding = "padding: var(--padding-top) var(--padding-right) var(--padding-bottom) var(--padding-left)";
+	text.className = "container"
 	
 	const link = document.createElement('a');
 	link.innerHTML = link.href = '/offline';
 	text.appendChild(link);
-	
+
 	text.appendChild(document.createTextNode(' to view cached pages offline.'));
 	
 	offline.appendChild(text);
