0131373daaa0c90c96b6041e37a6d1836cc9437f
Angelo Shema <geoangercola@gmail.com>
2021-03-20 16:25:22 +0200

    Edit intro info

--- components/welcome.js
+++ components/welcome.js
@@ -4,9 +4,9 @@ import Container from './container';
 let Welcome = () => {
 	return <div className="welcome">
 				<p className="description">
-					Hello, my name is Shema Angelo, but you can call me vixalien.
-					I am a web developer. This is my site, where you can find my
-					posts and projects. For more info links and feeds, go to <a href="/about">/about</a>
+					Hello! I'm Angelo Verlain, but you can call me vixalien.
+					I am a web developer. This is my website, a collection of projects and writings.<br/>
+					For more info, links and feeds go to <a href="/about">/about</a>
 				</p>
 				<p>
 					You can also view a <a href="/projects">list of my projects</a>
