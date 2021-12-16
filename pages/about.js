51833937774ef1207b2e35e4b79e29416884e6ce
Angelo Verlain <geoangercola@gmail.com>
2021-12-16 20:18:47 +0200

    Restructure about page

--- pages/about.js
+++ pages/about.js
@@ -20,43 +20,40 @@ export default () => {
 			<main>
 				<Intro title="About vixalien"/>
 				<p>
-					Hi! I'm Shema Angelo, or vixalien. I'm a web developer and (designer? maybe).
-					I love to build things from scratch while I can. Here you can find my projects,
-					posts and whatever. Stay tuned!
+					Hi! I'm Shema Angelo Verlain, or vixalien. I'm a web developer and student from Kigali, Rwanda.
+					I love to build random things that solve real world problems or just for fun.
+					I write code in JavaScript, HTML, CSS and use frameworks like React, NextJS and Explosiv.
+				</p>
+				<p>
+					This is a website where I will try to write posts about Web Development, Life and other experiences when I get free time from school.
+					Thanks for coming here! Have a nice day.
 				</p>
 				<h2>Links</h2>
 				<ul>
 					<li>
-						<a id="emailme">Loading (requires JavaScript btw, to foil spam bots)</a>
+						<a id="emailme">Loading Email (won't load if JavaScript is disabled)</a>
 						<script html={`
 							// foil spam bots
 							var email = 'hey';
-							email += '@vixalien.ga';
+							email += '@vixalien.com';
 							var emailme = document.getElementById('emailme')
 							emailme.href = 'mailto:'+email;
 							emailme.innerHTML = 'Email';
 						`}/>
 					</li>
 					<li><ExternalLink href="https://www.github.com/vixalien" text="Github"/></li>
-					<li><ExternalLink href="https://www.paypal.com/donate?hosted_button_id=SAAEAPL2GLXZS" text="Donate at Paypal"/></li>
 					<li><a href="/Resume.pdf">Resume.pdf</a></li>
 				</ul>
 				<h2>Experiments</h2>
 				<h3>Web bundle</h3>
 				<p>
-					A Web Bundle is a file format for encapsulating one or more HTTP resources in a single file. 
-					It can include one or more HTML files, JavaScript files, images, or stylesheets. 
-					Web Bundles, more formally known as Bundled HTTP Exchanges, are part of the Web Packaging proposal.
-					This is a web bundle of this site, last built at {new Date().toGMTString()}
+					A Web Bundle is an experimental file format for encapsulating one or more HTTP resources in a single file. 
+					This is a web bundle of this site, last built at around {new Date().toGMTString()}.
+					Note that to use the web bundle you may need to enable some flags or config in your browser.
 				</p>
 				<ul>
 					<li><a href="/vixalien.wbn">Download Web Bundle (vixalien.wbn)</a></li>
 				</ul>
-				<p>
-					Note that to use the web bundle you may need to enable some flags or config in your browser.
-					For Chrome go to <b>chrome://flags</b> and enable <b>Web Bundles</b>.
-					For Edge do the same thing at <b>edge://flags</b>. Firefox does not support web bundles AFAIK.
-				</p>
 				<h2>Feeds</h2>
 				<ul>
 					<li><a href="/feed/feed.rss">RSS</a></li>
