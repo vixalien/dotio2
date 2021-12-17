ef5fb078465df2ea1330c46cf9bc511d8e2b1189
Angelo Verlain <geoangercola@gmail.com>
2021-12-17 21:41:06 +0200

    Add carbon ads

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -24,11 +24,13 @@ const Post = ({
 			<Canonical path={'/post/'+slug}/>
 			<meta name="description" content={description} />
 			<link rel="stylesheet" href="/css/highlight.css" />
+			<link rel="stylesheet" href="/css/carbonads.css" />
 		</Head>
 		<Container tag="main">
 			<Header/>
 			<Intro title={title} created={new Date(created).toDateString()}/>
 			<p>{description}</p>
+			<script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?serve=CESIE2JN&placement=wwwvixaliencom" id="_carbonads_js"></script>
 			<hr/>
 			<br/>
 			<article html={content}/>
