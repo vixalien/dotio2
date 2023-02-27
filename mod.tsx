7ae37502d717aba002b9601f1190bb9ef9b75586
Angelo Verlain <geoangercola@gmail.com>
2023-02-27 17:29:21 +0200

    add mastodon link

--- mod.tsx
+++ mod.tsx
@@ -5,7 +5,43 @@ import blog, {
   highlight,
   imageContainer,
   redirects,
-} from "https://deno.land/x/vixalien_deno_blog@0.4.14/blog.tsx";
+} from "https://deno.land/x/vixalien_deno_blog@0.4.15/blog.tsx";
+
+const DocumentIcon = () => {
+  return (
+    <svg
+      width="1em"
+      height="1em"
+      stroke-width={2}
+      stroke="currentColor"
+      fill="none"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      viewBox="0 0 24 24"
+    >
+      <path d="M0 0h24v24H0z" stroke="none" />
+      <path d="M14 3v4a1 1 0 001 1h4" />
+      <path d="M17 21H7a2 2 0 01-2-2V5a2 2 0 012-2h7l5 5v11a2 2 0 01-2 2zM9 9h1M9 13h6M9 17h6" />
+    </svg>
+  );
+};
+
+const MastodonIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="ionicon"
+      viewBox="0 0 512 512"
+      width="1em"
+      height="1em"
+      stroke="currentColor"
+      fill="currentColor"
+    >
+      <title>Logo Mastodon</title>
+      <path d="M480 173.59c0-104.13-68.26-134.65-68.26-134.65C377.3 23.15 318.2 16.5 256.8 16h-1.51c-61.4.5-120.46 7.15-154.88 22.94 0 0-68.27 30.52-68.27 134.65 0 23.85-.46 52.35.29 82.59C34.91 358 51.11 458.37 145.32 483.29c43.43 11.49 80.73 13.89 110.76 12.24 54.47-3 85-19.42 85-19.42l-1.79-39.5s-38.93 12.27-82.64 10.77c-43.31-1.48-89-4.67-96-57.81a108.44 108.44 0 01-1-14.9 558.91 558.91 0 0096.39 12.85c32.95 1.51 63.84-1.93 95.22-5.67 60.18-7.18 112.58-44.24 119.16-78.09 10.42-53.34 9.58-130.17 9.58-130.17zm-80.54 134.16h-50V185.38c0-25.8-10.86-38.89-32.58-38.89-24 0-36.06 15.53-36.06 46.24v67h-49.66v-67c0-30.71-12-46.24-36.06-46.24-21.72 0-32.58 13.09-32.58 38.89v122.37h-50V181.67q0-38.65 19.75-61.39c13.6-15.15 31.4-22.92 53.51-22.92 25.58 0 44.95 9.82 57.75 29.48L256 147.69l12.45-20.85c12.81-19.66 32.17-29.48 57.75-29.48 22.11 0 39.91 7.77 53.51 22.92q19.79 22.72 19.75 61.39z" />
+    </svg>
+  );
+};
 
 blog({
   author: "Angelo Verlain",
@@ -17,8 +53,14 @@ blog({
   links: [
     { title: "Email", url: "mailto:hey@vixalien.com" },
     { title: "GitHub", url: "https://github.com/vixalien" },
+    {
+      title: "Mastodon",
+      url: "https://mas.to/@vixalientoots",
+      rel: "me",
+      icon: <MastodonIcon />,
+    },
     { title: "Twitter", url: "https://twitter.com/vixalientweets" },
-    { title: "Resume.pdf", url: "/Resume.pdf", icon: <IconFile /> },
+    { title: "Resume.pdf", url: "/Resume.pdf", icon: <DocumentIcon /> },
   ],
   canonicalUrl: Deno.env.get("URL"),
   description:
@@ -56,22 +98,3 @@ blog({
   ],
   lang: "en-US",
 });
-
-function IconFile() {
-  return (
-    <svg
-      width="1em"
-      height="1em"
-      stroke-width={2}
-      stroke="currentColor"
-      fill="none"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      viewBox="0 0 24 24"
-    >
-      <path d="M0 0h24v24H0z" stroke="none" />
-      <path d="M14 3v4a1 1 0 001 1h4" />
-      <path d="M17 21H7a2 2 0 01-2-2V5a2 2 0 012-2h7l5 5v11a2 2 0 01-2 2zM9 9h1M9 13h6M9 17h6" />
-    </svg>
-  );
-}
